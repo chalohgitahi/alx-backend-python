40940c9400615ccdf29cd5ccb1ff7f3fd1017601
chalohgitahi <gitahindegwa1@gmail.com>
2025-07-20 22:58:48 +0300

    update routes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
 # The router will automatically generate the URLs for the ConversationViewSet
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
-# We need a custom URL for the nested MessageViewSet
-message_list = MessageViewSet.as_view({'get': 'list', 'post': 'create'})
+conversations_router = nested_routers.NestedDefaultRouter(
+    router, r'conversations', lookup='conversation'
+)
+
+conversations_router.register(
+    r'messages', MessageViewSet, basename='conversation-messages'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path(
-        'conversations/<int:conversation_pk>/messages/',
-        message_list,
-        name='conversation-messages'
-    ),
+    path('', include(conversations_router.urls)),
 ]
