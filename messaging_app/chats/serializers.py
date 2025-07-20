e289cb0d64e762be4a73df781d6e869898b523d8
chalohgitahi <gitahindegwa1@gmail.com>
2025-07-20 18:47:52 +0300

    update serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,19 +1,32 @@
 from rest_framework import serializers
 from .models import User, Message, Conversation
 
+
 class UserSerializer(serializers.ModelSerializer):
+	email = serializers.CharField(
+        max_length=100,
+        help_text="The email has a (max 100 characters)."
+    )
 	class Meta:
 		model = User
-		fields = '__all__'
-
+		fields = ['user_id', 'username', 'email']
 
 class MessageSerializer(serializers.ModelSerializer):
+
+	last_activity = serializers.SerializerMethodField()
 	class Meta:
 		model = Message
-		fields = '__all__'
+		fields = ['message_id', 'sender_id', 'message_body', 'sent_at', 'last_activity']
+	
+	def get_last_activity(self, obj):
+		"""Returns the timestamp of the last message, or creation time if no messages."""
+		last_message = obj.messages.last()
+		if last_message:
+			return last_message.timestamp
+		return obj.created_at
 
 
 class ConversationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Conversation
-		fields = '__all__'
+		fields = ['conversation_id', 'message_id', 'created_at']
