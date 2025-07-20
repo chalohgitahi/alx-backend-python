5ed5436bb3d1c51eaf25dea7260ddb70cb7a58d7
chalohgitahi <gitahindegwa1@gmail.com>
2025-07-20 18:08:09 +0300

    update auth

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for alxtravelapp project.
+URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,30 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-# This sets up the schema for Swagger/OpenAPI documentation
-schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel App API",
-      default_version='v1',
-      description="API documentation for the ALX Travel App",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@alxtravel.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # URL patterns for the API documentation (Swagger and ReDoc)
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('chats.urls')),
 ]
