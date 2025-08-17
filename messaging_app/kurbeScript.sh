cbccf959d7a1522448800bb87d60112f75328354
chalohgitahi <gitahindegwa1@gmail.com>
2025-08-17 16:10:44 +0300

    Update kurbeScript.sh

--- messaging_app/kurbeScript.sh
+++ messaging_app/kurbeScript.sh
@@ -6,5 +6,10 @@ minikube start --driver=docker
 # Verify status of cluster
 minekube status
 
+kubectl cluster-info
+
 # see single node in cluster
 kubectl get nodeskurbeScript
+
+# Retrieve the available pods
+kubectl get pods
